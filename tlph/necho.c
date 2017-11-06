f39e6f9c2528d5b9e814af60f90a0fe4874af912
wuzhc <wuzhc2016@163.com>
2017-11-06 22:47:19 +0800

    necho

--- tlph/necho.c
+++ tlph/necho.c
@@ -3,9 +3,17 @@
 int main(int argc, char **argv)
 {
 	int i;
+	char **p;
 	for (i = 0;i < argc; i++) {
 		printf("argv[%d] = %s\n", i, argv[i]);
 	}
+
+	printf("或者这么做\n");
+
+	for (p = argv; *p != NULL; p++) {
+		puts(*p);
+	}
+
 	exit(EXIT_SUCCESS);
 	return 0;
 }
