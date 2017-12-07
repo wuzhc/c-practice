16e2dd69da2bd79a52cbcd929d175e97bc1e85cf
wuzhc <1716220125@qq.com>
2017-12-07 14:57:33 +0800

    system v restart ipc

---
+++ tlph/svmsg_demo_server.c
@@ -0,0 +1,36 @@
+#include <types.h>
+#include <ipc.h>
+#include <msg.h>
+#include <sys/stat.h>
+#include "tlpi_hdr.h"
+
+#define KEY_FILE = "/some-path/some-file"
+
+int main(int argc, char *argv[])
+{
+	key_t key;
+	int msgid;
+	const in MQ_PERMS = S_IRUSR | S_IWUSR | S_IRGRP;
+
+	key = ftok(KEY_FILE, 1);
+	if (key == -1) {
+		errExit("ftok failed \n");
+	}
+
+	/* 删除旧的IPC对象 */
+	while ((msgid = msgget(key, IPC_CREATE | IPC_EXCL | MQ_PERMS)) == -1) {
+		if (errno == EEXIST) { /* 已经存在 */
+			msgid = msgget(key, 0);
+			if (msgid == -1) {
+				errExit("msgget() failed to retrieve old queue ID");
+			}
+			if (msgctl(msgid, IPC_RMID, NULL) == -1) { /* 删除IPC对象 */
+				errExit("msgget() failed to delete old queue");
+			}
+			printf("removed old message queue (id=%d)\n", msgid); 
+		} else {
+			errExit("msgget failed");
+		}
+	}
+	return 0;
+}
