9e3bc863b85beef402b96f475a2227359ff3f2f7
wuzhc <1716220125@qq.com>
2017-12-07 10:21:14 +0800

    fork signal

---
+++ tlph/fork_signal.c
@@ -0,0 +1,42 @@
+#include <signal.h>
+#include "tlpi_hdr.h"
+
+/* 子进程退出SIGCHLD信号处理器 */
+void sigchldHandle(int sig)
+{
+	printf("caught signal %d \n", sig);
+}
+
+int main(int argc, char *argv[])
+{
+	pid_t pid;
+	int i;
+	struct sigaction sa;
+
+	/* 安装信号处理器 */
+	sigemptyset(&sa.sa_mask);
+	sa.flags = 0;
+	sa.sa_handler = sigchldHandle;
+	if (sigaction(SIGCHLD, &sa, NULL) == -1) {
+		errExit("sigaction failed \n");
+	}
+
+	/* 创建子进程 */
+	printf("parent pid = %d", getppid());
+
+	for (i=0; i<5; i++) {
+		switch(pid = fork()){
+			case -1:
+				errExit("fork failed \n");
+				break;
+			case 0:
+				printf("child pid = %d\n", getppid());
+				_exit(EXIT_SUCCESS);
+				break;
+			default:
+				break;
+		}
+	}
+
+	return 0;
+}
