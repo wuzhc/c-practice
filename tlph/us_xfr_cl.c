871531bb62e9d46b0495f2b629861e0ff32ae3d7
wuzhc <1716220125@qq.com>
2017-12-18 17:28:52 +0800

    us_xfr_cl.c

--- tlph/us_xfr_cl.c
+++ tlph/us_xfr_cl.c
@@ -2,6 +2,33 @@
 
 int main(int argc, char *argv[])
 {
-	
+	int sfd;
+	ssize_t numRead;
+	struct sockaddr_un addr;
+	char buf[BUF_SIZE];
+
+	sfd = socket(AF_UNIX, SOCK_STREAM, 0);
+	if (sfd == -1) {
+		errExit("socket failed \n");
+	}
+
+	memset(&addr, 0,  sizeof(struct sockaddr_un));
+	addr.sun_family = AF_UNIX;
+	strncpy(addr.sun_path, SV_SOCK_PATH, sizeof(addr.sun_path) - 1);
+
+	if (connect(sfd, (struct sockaddr *)&addr, sizeof(struct sockaddr_un)) == -1) {
+		errExit("connect failed\n");
+	}
+
+	while (numRead = read(STDIN_FILENO, buf, BUF_SIZE) > 0) {
+		if (write(sfd, buf, numRead) != numRead) {
+			errExit("partial write \n");
+		}
+	}
+
+	if (numRead == -1) {
+		errExit("read failed \n");
+	}
+
 	return 0;
 }
--- tlph/us_xfr_sv.c
+++ tlph/us_xfr_sv.c
@@ -20,9 +20,9 @@ int main(int argc, char *argv[])
 
 	memset(&addr, 0, sizeof(struct sockaddr_un)); /* 清空sockaddr_un数据结构为0 */
 	addr.sun_family = AF_UNIX;
-	strncpy(addr.sun_path, SV_SOCK_PATH, strlen(addr.sun_path) - 1); /* 将SV_SOCK_PATH复制到addr.sun_path */
+	strncpy(addr.sun_path, SV_SOCK_PATH, sizeof(addr.sun_path) - 1); /* 将SV_SOCK_PATH复制到addr.sun_path */
 
-	if (bind(sfd, (struct sockaddr *)&addr, strlen(struct sockaddr_un)) == -1) {
+	if (bind(sfd, (struct sockaddr *)&addr, sizeof(struct sockaddr_un)) == -1) {
 		errExit("bind failed \n");
 	}
 
